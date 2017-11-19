b63956a684842f627051b535e84744fcb6e53fde
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-19 01:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName)
+function upperCaseCustomerName()
 {
   return customerName.toUpperCase();
 }
